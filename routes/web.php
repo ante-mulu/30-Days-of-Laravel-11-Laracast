596e9bf4865349ea616319bdbce8ac6c4da3af05
Your Name <you@example.com>
2024-06-11 19:59:45 +0300

    Crud

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 use App\Http\Controllers\JobController;
+
+Route::get('/',function(){return view('home');});
 Route::resource("jobs", JobController::class);
 Route::get('/about',function(){return view('about');});
 Route::get('/contact',function(){return view('contact');});
